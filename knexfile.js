c36f825d39863b4e3d9be01cfb91ee7ccda4a9ba
AndrewA0112 <andrew.mkal@outlook.com>
2019-09-23 14:02:06 -0500

    Deleted useNullAsDefault from knexfile

--- knexfile.js
+++ knexfile.js
@@ -24,7 +24,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
